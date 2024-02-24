e4a4c41a23d845a8fd7899523cff388973b8708c
wowrona <wowrona@snf4j.org>
2024-02-24 10:51:08 +0100

    Fix test

--- snf4j-core/src/test/java/org/snf4j/core/SessionTest.java
+++ snf4j-core/src/test/java/org/snf4j/core/SessionTest.java
@@ -2514,6 +2514,7 @@ public class SessionTest {
 
 			session = c.getSession();
 			session.suspendWrite();
+			waitFor(100);
 			for (int i=0; i<writeCount; i++) {
 				sessionWrite(session, data, t, i);
 			}
@@ -2540,6 +2541,7 @@ public class SessionTest {
 
 			session = c.getSession();
 			session.suspendWrite();
+			waitFor(100);
 			for (int i=0; i<writeCount; i++) {
 				sessionWrite(session, data, t, i);
 			}
@@ -2589,6 +2591,7 @@ public class SessionTest {
 		((TestStreamSession)session).getOutBuffersException = true;
 		session = c.getSession();
 		session.suspendWrite();
+		waitFor(100);
 		for (int i=0; i<writeCount; i++) {
 			sessionWrite(session, data, 0, i);
 		}
@@ -2612,6 +2615,7 @@ public class SessionTest {
 		((TestStreamSession)session).getOutBuffersExceptionDelay = 1;
 		session = c.getSession();
 		session.suspendWrite();
+		waitFor(100);
 		for (int i=0; i<writeCount; i++) {
 			sessionWrite(session, data, 0, i);
 		}
@@ -2633,6 +2637,7 @@ public class SessionTest {
 		
 		session = c.getSession();
 		session.suspendWrite();
+		waitFor(100);
 		session.write(new Packet(PacketType.NOP, "123456").toBytes());
 		TestSelectionKey key = new TestSelectionKey(new TestSocketChannel());
 		Method m = SelectorLoop.class.getDeclaredMethod("handleWriting", StreamSession.class, SelectionKey.class, int.class);
